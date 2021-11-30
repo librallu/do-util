9109245af74c46498c0eebad38a1d25509ed6b9c
Luc Libralesso <libralesso.l@gmail.com>
2021-11-30 23:48:28 +0100

    improve memory usage of the trie

--- src/set_store/set_trie.rs
+++ src/set_store/set_trie.rs
@@ -9,6 +9,86 @@ struct Node {
     children:Vec<Option<Box<Node>>>,
     /// number of "active" children
     nb_children:usize,
+    /// offset of the children
+    offset:usize,
+}
+
+impl Node {
+    /// creates a new empty node
+    pub fn new() -> Self {
+        Node {
+            contains_set:false,
+            children:vec![],
+            nb_children:0,
+            offset: 0
+        }
+    }
+
+    /// returns true iff the given value is out of bounds
+    pub fn is_out_of_bounds(&self, v:usize) -> bool {
+        v < self.offset || v >= self.offset+self.children.len()
+    }
+
+    /// returns the child of the node using 
+    pub fn child(&self, v:usize) -> &Option<Box<Node>> {
+        &self.children[v-self.offset]
+    }
+
+    /// returns the (mutable) child of the node using 
+    pub fn child_mut(&mut self, v:usize) -> &mut Option<Box<Node>> {
+        &mut self.children[v-self.offset]
+    }
+
+    /// adds a child to the node if it does not exist.
+    /// returns it
+    pub fn add_child(&mut self, v:usize) -> &mut Node{
+        assert!(v >= self.offset);
+        // add new children
+        while self.offset+self.children.len() <= v {
+            self.children.push(None);
+        }
+        // update current node and index
+        let children_index = v-self.offset;
+        if self.children[children_index].is_none() {
+            self.nb_children += 1;
+            self.children[children_index] = Some(Box::new(Node {
+                contains_set:false,
+                children:vec![],
+                nb_children:0,
+                offset: v+1
+            }));
+        }
+        &mut (*self.children[children_index].as_mut().unwrap())
+    }
+
+    /// removes a child indexed by v
+    fn remove_child(&mut self, v:usize) -> bool {
+        if self.is_out_of_bounds(v) { return false; }
+        if v-self.offset >= self.children.len() { return false; }
+        self.children[v-self.offset] = None;
+        true
+    }
+
+    /// adds a set to the node. Returns true if the set was successfully added
+    fn add_set(&mut self) -> bool {
+        if self.contains_set {
+            false
+        } else {
+            self.contains_set = true;
+            true
+        }
+    }
+
+    /// returns true iff the node contains a set
+    fn has_set(&self) -> bool { self.contains_set }
+
+    /// removes the set if it exists
+    fn remove_set(&mut self) {
+        self.contains_set = false;
+    }
+
+    /// returns the number of children
+    fn nb_children(&self) -> usize { self.children.len() }
 }
 
 /// Set Trie.
@@ -26,138 +106,110 @@ impl<T:Copy+Eq+Into<usize>+From<usize>> SetStore<T> for TrieSetStore {
     type SupersetIterator = std::vec::IntoIter<Vec<T>>;
 
     fn insert(&mut self, s:&[T]) -> bool {
-        let mut index:usize = 0;
-        let mut current_node = &mut self.root;
-        while index < s.len() {
-            let e = s[index];
-            if current_node.is_none() { // populate the node if needed
-                *current_node = Some(Box::new(Node {
-                    contains_set:false,
-                    children:vec![],
-                    nb_children:0,
-                }));
-            }
-            // extend the children until e is found
-            let n = current_node.as_mut().unwrap();
-            while n.children.len() <= e.into() {
-                n.children.push(None);
-            }
-            // update current node and index
-            if n.children[e.into()].is_none() {
-                n.nb_children += 1;
-            }
-            current_node = &mut n.children[e.into()];
-            index += 1;
+        let mut current_node = &mut **self.root.as_mut().unwrap();
+        for v in s {
+            current_node = current_node.add_child((*v).into());
         }
-        if current_node.is_none() { // populate the node if needed
-            *current_node = Some(Box::new(Node {
-                contains_set:false,
-                children:vec![],
-                nb_children:0
-            }));
-        }
-        if current_node.as_ref().unwrap().contains_set { return false; }
-        current_node.as_mut().unwrap().contains_set = true;
-        true
+        current_node.add_set()
     }
 
     fn remove(&mut self, s:&[T]) -> bool {
-        Self::remove_rec(&mut self.root, s, 0)
+        Self::remove_rec(self.root.as_mut().unwrap(), s, 0)
     }
 
     fn find_subsets(&self, s:&[T]) -> Self::SubsetIterator {
-        Self::rec_find_subsets(&self.root, s, 0, Vec::new()).into_iter()
+        Self::rec_find_subsets(
+            self.root.as_ref().unwrap(),
+            s,
+            0,
+            Vec::new()
+        ).into_iter()
     }
 
     fn find_supersets(&self, s:&[T]) -> Self::SupersetIterator {
-        Self::rec_find_supersets(&self.root, s, 0, Vec::new()).into_iter()
+        Self::rec_find_supersets(
+            self.root.as_ref().unwrap(),
+            s,
+            0,
+            Vec::new()
+        ).into_iter()
     }
 
     fn contains(&self, s:&[T]) -> bool {
-        let mut current_node = &self.root;
-        for e in s {
-            match current_node {
-                None => { return false; }
-                Some(n) => {
-                    if n.children.len() < (*e).into() { return false; }
-                    current_node = &n.children[(*e).into()]
-                }
+        let mut current_node = self.root.as_ref().unwrap();
+        for v in s {
+            if current_node.is_out_of_bounds((*v).into()) { return false; }
+            match current_node.child((*v).into()) {
+                None => { return false; },
+                Some(n) => { current_node = n; },
             }
         }
-        match current_node {
-            None => { false }
-            Some(n) => { n.contains_set }
-        }
+        current_node.has_set()
     }
 }
 
 impl Default for TrieSetStore {
     fn default() -> Self {
-        Self { root: Some(Box::new(Node {
-            contains_set: false,
-            children: Vec::new(),
-            nb_children: 0
-        })) }
+        Self { root: Some(Box::new(Node::new())) }
     }
 }
 
 
 impl TrieSetStore {
 
-    fn rec_find_subsets<T:Copy+Eq+Into<usize>>(node:&Option<Box<Node>>, e:&[T], index:usize, selected:Vec<T>)
+    fn rec_find_subsets<T:Copy+Eq+Into<usize>+From<usize>>(node:&Node, e:&[T], index:usize, selected:Vec<T>)
     -> Vec<Vec<T>> {
         let mut res = Vec::new();
-        match node {
-            None => { return res; }
-            Some(n) => {
-                if n.contains_set { res.push(selected.clone()); }
-                for i in index..e.len() {
-                    let v = e[i];
-                    if v.into() < n.children.len() && n.children[v.into()].is_some() {
-                        let mut new_selected = selected.clone();
-                        new_selected.push(v);
-                        let mut rec_res = Self::rec_find_subsets(
-                            &n.children[v.into()], e, i, new_selected.clone()
-                        );
-                        res.append(&mut rec_res);
-                    }
+        if node.contains_set { res.push(selected.clone()); }
+        for i in index..e.len() {
+            let v = e[i];
+            if node.is_out_of_bounds(v.into()) { continue; }
+            match node.child(v.into()) {
+                None => { continue; }
+                Some(child) => {
+                    let mut new_selected = selected.clone();
+                    new_selected.push(i.into());
+                    let mut rec_res = Self::rec_find_subsets(
+                        child, e, i, new_selected.clone()
+                    );
+                    res.append(&mut rec_res);
                 }
             }
         }
         res
     }
 
-    fn rec_find_supersets<T:Copy+Eq+Into<usize>+From<usize>>(node:&Option<Box<Node>>, e:&[T], index:usize, selected:Vec<T>)
+    fn rec_find_supersets<T:Copy+Eq+Into<usize>+From<usize>>(node:&Node, e:&[T], index:usize, selected:Vec<T>)
     -> Vec<Vec<T>> {
         let mut res = Vec::new();
-        match node {
-            None => { return res; },
-            Some(n) => {
-                if index >= e.len() { // find all sets below
-                    if n.contains_set { res.push(selected.clone()); }
-                    for (i,c) in n.children.iter().enumerate() {
+        if index >= e.len() { // find all sets below
+            if node.contains_set { res.push(selected.clone()); }
+            for (i,c) in node.children.iter().enumerate()
+            .filter(|(_,c)| c.is_some()) {
+                let mut new_selected = selected.clone();
+                new_selected.push(T::from(i+node.offset));
+                let mut rec_res = Self::rec_find_supersets(c.as_ref().unwrap(),e,index,new_selected);
+                res.append(&mut rec_res);
+            }
+        } else { // iterate over all values less or equal than e[index]
+            let v = e[index];
+            for i in 0..v.into()+1 { // search for all elements below the value v (included)
+                if node.is_out_of_bounds(i) { continue; }
+                match node.child(i) {
+                    None => { continue; },
+                    Some(child) => {
                         let mut new_selected = selected.clone();
                         new_selected.push(i.into());
-                        let mut rec_res = Self::rec_find_supersets(c,e,index,new_selected);
+                        let mut rec_res = if i == v.into() {
+                            Self::rec_find_supersets(
+                                child, e, index+1, new_selected.clone()
+                            )
+                        } else {
+                            Self::rec_find_supersets(
+                                child, e, index, new_selected.clone()
+                            )
+                        };
                         res.append(&mut rec_res);
-                    }
-                } else { // iterate over all values less or equal than e[index]
-                    let v = e[index];
-                    for i in 0..v.into()+1 {
-                        if i < n.children.len() && n.children[i].is_some() {
-                            let mut new_selected = selected.clone();
-                            new_selected.push(i.into());
-                            let mut rec_res = if i == v.into() {
-                                Self::rec_find_supersets(
-                                    &n.children[i], e, index+1, new_selected.clone()
-                                )
-                            } else {
-                                Self::rec_find_supersets(
-                                    &n.children[i], e, index, new_selected.clone()
-                                )
-                            };
-                            res.append(&mut rec_res);
-                        }
                     }
                 }
             }
@@ -181,7 +233,9 @@ impl TrieSetStore {
         let mut current_id = id+1;
         for (v,child) in node.children.iter().enumerate() {
             if let Some(c) = child {
-                res += format!("\t{} -> {} [label=\"{}\"];\n", id, current_id, v).as_str();
+                res += format!("\t{} -> {} [label=\"{}\"];\n",
+                    id, current_id, v+child.as_ref().unwrap().offset
+                ).as_str();
                 let (tmp_str, next_id) = Self::node_to_graphviz(c, current_id);
                 res += tmp_str.as_str();
                 current_id = next_id;
@@ -191,37 +245,23 @@ impl TrieSetStore {
     }
 
     /// removes e[index:] from node
-    fn remove_rec<T:Copy+Eq+Into<usize>>(node:&mut Option<Box<Node>>, e:&[T], index:usize) -> bool {
+    fn remove_rec<T:Copy+Eq+Into<usize>>(node:&mut Node, e:&[T], index:usize) -> bool {
         if index == e.len() { // right spot is found
-            match node {
-                None => { return false; }
-                Some(n) => {
-                    if n.contains_set {
-                        if n.nb_children == 0 { // if no children, remove the node,
-                            *node = None;
-                        } else {// otherwise, just remove the set of the node
-                            n.contains_set = false;
-                        }
-                        return true;
-                    } else { return false; } // the right spot does not contain a set
-                }
-            }
-        } else if index >= e.len() { return false; } // no element found
-        match node {
-            None => { false }
-            Some(n) => {
-                let v:usize = e[index].into();
-                if n.children.len() <= v { false }
-                else { // call recursive remove
-                    let child_is_some = n.children[v].is_some();
-                    let res = Self::remove_rec(&mut n.children[v], e, index+1);
-                    if child_is_some && n.children[v].is_none() { // child removed
-                        n.nb_children -= 1;
-                        if n.nb_children == 0 {
-                            *node = None;
-                        }
+            if node.has_set() { node.remove_set(); true }
+            else { false }
+        } else {
+            // check if the next element exists, and perform a recursive remove
+            if node.is_out_of_bounds(e[index].into()) { return false; }
+            match node.child_mut(e[index].into()) {
+                None => { false }
+                Some(child) => { // if the child exists, recursive call
+                    let res = Self::remove_rec(&mut **child, e, index+1);
+                    if !res { return false; }
+                    // if an element was removed, check its children, and possibly remove it
+                    if child.nb_children() == 0 && !child.has_set() {
+                        node.remove_child(e[index].into());
                     }
-                    res
+                    true
                 }
             }
         }
